0ceba68dd83a9144134e95576a06e8e7286bce03
Tom Porter <tomporter62@gmail.com>
2020-06-15 15:14:00 +0100

    Improved formatting of the SQL query

--- Database/CreateValorantDatabase.sql
+++ Database/CreateValorantDatabase.sql
@@ -14,19 +14,22 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Maps')
 	DROP TABLE Maps
 GO
 
-CREATE TABLE "Maps"(
+CREATE TABLE "Maps"
+(
 	MapID int IDENTITY(1,1),
 	MapName varchar(20) NOT NULL
 	CONSTRAINT PK_MapID PRIMARY KEY(MapID)
 );
 
-CREATE TABLE "AgentType"(
+CREATE TABLE "AgentType"
+(
 	TypeID int IDENTITY(1,1),
 	TypeName varchar(20) NOT NULL
 	CONSTRAINT PK_TypeID PRIMARY KEY(TypeID)
 );
 
-CREATE TABLE "Agents"(
+CREATE TABLE "Agents"
+(
 	AgentID int IDENTITY(1,1),
 	AgentName varchar(20) NOT NULL,
 	AgentTypeID int NOT NULL,
@@ -43,7 +46,8 @@ CREATE TABLE "Agents"(
 	CONSTRAINT FK_AgentType FOREIGN KEY (AgentTypeID) REFERENCES AgentType(TypeID)
 );
 
-CREATE TABLE "GameLogs"(
+CREATE TABLE "GameLogs"
+(
 	GameID int IDENTITY(1,1) NOT NULL,
 	MapID int NOT NULL,
 	AgentID int NOT NULL,
@@ -57,9 +61,25 @@ CREATE TABLE "GameLogs"(
 	CONSTRAINT PK_GameLogs PRIMARY KEY (GameID),
 	CONSTRAINT FK_Map FOREIGN KEY (MapID) REFERENCES Maps(MapID),
 	CONSTRAINT FK_Agent FOREIGN KEY (AgentID) REFERENCES Agents(AgentID),
-	CONSTRAINT ValidTeamScore CHECK(TeamScore >= 0 AND TeamScore <= 13),
-	CONSTRAINT ValidOpponentScore CHECK(OpponentScore >= 0 AND OpponentScore <= 13),
-	CONSTRAINT ValidKills CHECK(Kills >= 0),
-	CONSTRAINT ValidDeaths CHECK(Deaths >= 0),
-	CONSTRAINT ValidAssits CHECK(Assits >= 0)
+	CONSTRAINT ValidTeamScore CHECK
+	(
+		TeamScore >= 0 AND 
+		TeamScore <= 13
+	),
+	CONSTRAINT ValidOpponentScore CHECK
+	(
+		OpponentScore >= 0 AND 
+		OpponentScore <= 13
+	),
+	CONSTRAINT ValidKills CHECK
+	(
+		Kills >= 0
+	),
+	CONSTRAINT ValidDeaths CHECK
+	(
+		Deaths >= 0
+	),
+	CONSTRAINT ValidAssits CHECK(
+		Assits >= 0
+	)
 )
