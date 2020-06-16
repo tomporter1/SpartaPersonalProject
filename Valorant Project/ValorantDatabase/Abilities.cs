4e8562d438822bd3d81c367fbe38c00fdea07339
Tom Porter <tomporter62@gmail.com>
2020-06-16 16:17:44 +0100

    Started work on abilities EF class but haven't migrated the database yet

--- Valorant Project/ValorantDatabase/Abilities.cs
+++ Valorant Project/ValorantDatabase/Abilities.cs
@@ -8,11 +8,15 @@ namespace ValorantDatabase
     {
         public Abilities()
         {
-
+            Agents = new HashSet<Agents>();
         }
 
         public int AbilitytId { get; set; }
         public string AbilityName { get; set; }
+        public string AbilityDiscription { get; set; }
+        public int Cost { get; set; }
+        public int MaxCharges { get; set; }
+        public virtual ICollection<Agents> Agents { get; set; }
 
     }
 }
