0cb4a96b51668a0248b115577831e001f03b8693
Tom Porter <tomporter62@gmail.com>
2020-06-24 17:37:30 +0100

    Added Equals overrides to GameMode Class

--- Valorant Project/ValorantDatabase/PartialClasses/GameModes.cs
+++ Valorant Project/ValorantDatabase/PartialClasses/GameModes.cs
@@ -4,10 +4,36 @@ using System.Text;
 
 namespace ValorantDatabase
 {
-    public partial class GameModes
+    public partial class GameModes : IEquatable<GameModes>
     {
         private int _modeID;
         private string _modeName;
         private string _modeDiscription;
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as GameModes);
+        }
+
+        public bool Equals(GameModes other)
+        {
+            return other != null &&
+                   _modeID == other._modeID;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_modeID);
+        }
+
+        public static bool operator ==(GameModes left, GameModes right)
+        {
+            return EqualityComparer<GameModes>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(GameModes left, GameModes right)
+        {
+            return !(left == right);
+        }
     }
 }
