bc2fb002a80ea5a0d19fce290a28224f76e295d2
Tom Porter <tomporter62@gmail.com>
2020-09-02 15:52:54 +0100

    Changed gamelogs.ToString()

--- Valorant Project/ValorantDatabase/PartialClasses/GameLogs.cs
+++ Valorant Project/ValorantDatabase/PartialClasses/GameLogs.cs
@@ -53,7 +53,7 @@ namespace ValorantDatabase
             Agents agent = db.GameLogs.Where(gl => gl.GameId == this.GameId).Include(gl => gl.Agent).Select(gl => gl.Agent).FirstOrDefault();
             Ranks rank = db.GameLogs.Where(gl => gl.RankID == this.RankID).Include(gl => gl.Rank).Select(gl => gl.Rank).FirstOrDefault();
 
-            return $"{GameResult} on {map} as {agent} - Season: {Season}{(RankID == null ? "" : " " + rank.RankName)}";
+            return $"{GameResult} on {map} as {agent} - Season: {Season}{(RankID == null ? "" : $" {rank}")}";
         }
 
         public static bool operator ==(GameLogs left, GameLogs right)
